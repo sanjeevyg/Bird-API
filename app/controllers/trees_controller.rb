a99284e2d5a28140bcd8bc51b5ea81c9c80e81f9
sanjeevyg <yogi.sjv@gmail.com>
2020-08-27 20:15:59 -0600

    Index and Show action added for Tree

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,2 +1,11 @@
 class TreesController < ApplicationController
+    def index 
+        @trees = Tree.all
+        render json: @trees, include: [:birds]
+    end
+
+    def show 
+        @tree = Tree.find(params[:id])
+        render json: @tree
+    end
 end
